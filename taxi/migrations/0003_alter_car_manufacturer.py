fa15c470f2b631ab04039ae7a162d1a5b061215c
Alexandr <oleksandrglushen2005@gmail.com>
2025-07-15 20:39:41 +0300

    fixed code 5

--- taxi/migrations/0003_alter_car_manufacturer.py
+++ taxi/migrations/0003_alter_car_manufacturer.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='car',
             name='manufacturer',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='cars', to='taxi.manufacturer', unique=True),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='cars', to='taxi.manufacturer'),
         ),
     ]
